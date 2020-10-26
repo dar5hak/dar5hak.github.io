6211e0e785ba5e31fb293ccfb94bf8bf475bedc3
Darshak Parikh <darshak@protonmail.com>
2020-10-26 22:35:33 +0530

    Persist button behaviour across page loads

--- static/scripts/restyle.js
+++ static/scripts/restyle.js
@@ -4,14 +4,23 @@ const styles = cartwheel(["default-style", "hacker-style"]);
 
 let currentStyle = styles.nextValue();
 
-document.querySelector(".buttons").classList.remove("is-hidden");
-
-document.querySelector("#lucky").addEventListener("click", () => {
+function changeStyle() {
   const newStyle = styles.nextValue();
   document.body.classList.replace(currentStyle, newStyle);
   currentStyle = newStyle;
-});
+}
+
+document.querySelector(".buttons").classList.remove("is-hidden");
+
+document.querySelector("#lucky").addEventListener("click", changeStyle);
 
 document.addEventListener("turbolinks:before-render", (event) => {
-  event.data.newBody.classList.add(currentStyle);
+  const { newBody } = event.data;
+  newBody.classList.add(currentStyle);
+
+  const buttons = newBody.querySelector(".buttons");
+  if (buttons) buttons.classList.remove("is-hidden");
+
+  const lucky = newBody.querySelector("#lucky");
+  if (lucky) lucky.addEventListener("click", changeStyle);
 });
